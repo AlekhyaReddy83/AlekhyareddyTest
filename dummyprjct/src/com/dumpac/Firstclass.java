95846616891d44243849469e225856b5aec5cf13
AlekhyaReddy83 <40258088+AlekhyaReddy83@users.noreply.github.com>
2019-08-01 11:32:02 +0530

    Revert "new added line"
    This reverts commit 060a8c022405a29278242371ec99fb67e9ad66c6.

--- dummyprjct/src/com/dumpac/Firstclass.java
+++ dummyprjct/src/com/dumpac/Firstclass.java
@@ -5,7 +5,6 @@ public class Firstclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Firstclass");
-		System.out.println("I'm 2nd line");
 	}
 
 }
