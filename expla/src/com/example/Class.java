e28d563ac698d7823096bf33605df898cfc6ebf5
AlekhyaReddy83 <40258088+AlekhyaReddy83@users.noreply.github.com>
2019-08-01 12:18:17 +0530

    eaxmple

---
+++ expla/src/com/example/Class.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Class {
+	int x,y,z;
+	public void add() {
+		
+		z=x+y;
+	}
+	public void sub() {
+		z=x-y;
+	}
+}
---
+++ expla/src/com/example/Class2.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter something");
+		Class2 a = new Class2();
+		a.add();
+		a.sub();
+	}
+
+}
